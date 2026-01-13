bf00aa3a2d3ae637cd88c34af3b51d3164e7ef0f
Januth Nimnal <nimnaljanuth@gmail.com>
2026-01-13 22:39:41 +0530

    Add files

---
+++ Orin AI - an AI for Sri Lankans-win32-x64/mobile-capacitor/android/app/src/test/java/com/getcapacitor/myapp/ExampleUnitTest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4045ae8fac1f0807e6b24eb34e68409a9a235df9d1a3f896fb8c9a49424363d
+size 402
---
+++ Orin AI - an AI for Sri Lankans-win32-x64/resources/app/lib/static/login.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5bf0423d12486786aee6fe385f180086063995df4a5dc2463b5d05a7d016679f
+size 481
